a4964e9b73ed356fa6fa53b0d99a2d8b51e7f58e
lianahus <liana@ethereum.org>
2022-06-17 17:35:19 +0200

    renamed envs

--- libs/remix-ui/run-tab/src/lib/reducers/runTab.ts
+++ libs/remix-ui/run-tab/src/lib/reducers/runTab.ts
@@ -114,27 +114,27 @@ export const runTabInitialState: RunTabState = {
       title: 'Execution environment does not connect to any node, everything is local and in memory only.',
       value: 'vm-london',
       fork: 'london',
-      content: 'JavaScript VM (London)'
+      content: 'Local VM (London)'
     }, {
       id: 'vm-mode-berlin',
       dataId: 'settingsVMBerlinMode',
       title: 'Execution environment does not connect to any node, everything is local and in memory only.',
       value: 'vm-berlin',
       fork: 'berlin',
-      content: 'JavaScript VM (Berlin)'
+      content: 'Local VM (Berlin)'
     }, {
       id: 'injected-mode',
       dataId: 'settingsInjectedMode',
       title: 'Execution environment has been provided by Metamask or similar provider.',
       value: 'injected',
-      content: 'Injected Web3'
+      content: 'Injected Provider - Metamask'
     }, {
       id: 'web3-mode',
       dataId: 'settingsWeb3Mode',
       title: `Execution environment connects to node at localhost (or via IPC if available), transactions will be sent to the network and can cause loss of money or worse!
       If this page is served via https and you access your node via http, it might not work. In this case, try cloning the repository and serving it via http.`,
       value: 'web3',
-      content: 'Web3 Provider'
+      content: 'JSON-RPC Provider'
     }],
     isRequesting: false,
     isSuccessful: false,
